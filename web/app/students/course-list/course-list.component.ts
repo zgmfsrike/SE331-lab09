2a789d63b98cc092560f4dc563bf3d582585d6ae
nuBoreZ <kritsanah_supphasri@cmu.ac.th>
2017-11-02 23:11:03 +0700

    2.5 Done!

--- web/app/students/course-list/course-list.component.ts
+++ web/app/students/course-list/course-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from "@angular/core";
+import {Component, Input, OnInit} from "@angular/core";
 
 @Component({
 selector : 'course-list',
@@ -7,8 +7,8 @@ styleUrls : ['app/students/course-list/course-list.component.css']
 })
 export class CourseListComponent implements OnInit{
   constructor(){
-
   }
+  @Input() count:number;
   ngOnInit(){
 
   }
